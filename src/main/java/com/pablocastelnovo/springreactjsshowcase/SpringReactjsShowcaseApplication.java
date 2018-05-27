7e1d0263479dc8ef6f3131929406e176a8756583
Pablo Castelnovo <pablonvz@gmail.com>
2018-05-27 18:56:33 -0400

    Add base spring boot config

---
+++ src/main/java/com/pablocastelnovo/springreactjsshowcase/SpringReactjsShowcaseApplication.java
@@ -0,0 +1,12 @@
+package com.pablocastelnovo.springreactjsshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactjsShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactjsShowcaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pablocastelnovo/springreactjsshowcase/SpringReactjsShowcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pablocastelnovo.springreactjsshowcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReactjsShowcaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
