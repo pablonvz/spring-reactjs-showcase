300671ab5df24a8b09a9a756c253e7b0d36d739b
Pablo Castelnovo <pablonvz@gmail.com>
2018-06-02 21:23:45 -0400

    Rename spring application

--- src/main/java/com/pablocastelnovo/springreactjsshowcase/Application.java
+++ src/main/java/com/pablocastelnovo/springreactjsshowcase/Application.java
@@ -6,11 +6,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-    FileSystemContentRepositoryProperties.class
+        FileSystemContentRepositoryProperties.class
 })
-public class SpringReactjsShowcaseApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringReactjsShowcaseApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
