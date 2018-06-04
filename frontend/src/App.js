7de786681d3b2a851722717801ff5a2d5f560294
Pablo Castelnovo <pablonvz@gmail.com>
2018-06-04 01:52:46 -0400

    Add production build of the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import FileManager from './FileManager'
 
-const basePath = 'http://127.0.0.1:8080/api/files/';
+// const basePath = 'http://127.0.0.1:8080/api/files/';
+const basePath = window.location.origin + '/api/files/';
 
 const App = () => (
     <div className="container">
--- src/main/java/com/pablocastelnovo/springreactjsshowcase/FileMetadataController.java
+++ src/main/java/com/pablocastelnovo/springreactjsshowcase/FileMetadataController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/files")
+@RequestMapping("/api/files")
 @CrossOrigin(origins = "http://localhost:3000")
 public class FileMetadataController {
 
